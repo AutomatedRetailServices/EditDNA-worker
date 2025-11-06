3ca114e3765d2d0c482472b97d6702ac0f401c12
AutomatedRetailServices <arsgroup01@gmail.com>
2025-11-06 11:32:18 -0500

    Update __init__.py

--- worker/__init__.py
+++ worker/__init__.py
@@ -1,20 +1,8 @@
-"""
-Worker helpers package.
+# /workspace/EditDNA-worker/worker/__init__.py
 
-We only expose the helper modules that actually exist.
-Do NOT import pipeline here.
-"""
-
-from . import s3
-from . import asr
-from . import sentence_boundary
-from . import semantic_visual_pass
+# Make submodules visible when someone does: from worker import video
+from . import video
 from . import vision_sampler
 
-__all__ = [
-    "s3",
-    "asr",
-    "sentence_boundary",
-    "semantic_visual_pass",
-    "vision_sampler",
-]
+# If later you add s3 or ffmpeg utils, you can import them here too.
+__all__ = ["video", "vision_sampler"]
