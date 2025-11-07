834cbdad0ce28e882c2e9da8adf98676d5a00886
AutomatedRetailServices <arsgroup01@gmail.com>
2025-11-07 10:45:33 -0500

    Update s3.py

--- worker/s3.py
+++ worker/s3.py
@@ -2,25 +2,33 @@
 import os
 import boto3
 
-# you can override these in the pod env
+# env-configurable
 S3_BUCKET = os.environ.get("S3_BUCKET", "script2clipshop-video-automatedretailservices")
 AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
 
 
-def upload_file(local_path: str, key: str) -> str:
+def upload_file(local_path: str, key: str):
     """
-    Uploads local_path to S3_BUCKET at object key.
-    Returns an https URL so the API can hand it to the frontend.
+    Upload local_path to S3 at object 'key'.
+
+    Returns a tuple:
+        (s3_url, https_url)
+
+    so the pipeline can fill both fields.
     """
     session = boto3.session.Session()
     s3_client = session.client("s3", region_name=AWS_REGION)
 
+    # do the upload
     s3_client.upload_file(local_path, S3_BUCKET, key)
 
-    # URL style
-    if AWS_REGION == "us-east-1":
-        url = f"https://{S3_BUCKET}.s3.amazonaws.com/{key}"
-    else:
-        url = f"https://{S3_BUCKET}.s3.{AWS_REGION}.amazonaws.com/{key}"
+    # s3-style
+    s3_url = f"s3://{S3_BUCKET}/{key}"
 
-    return url
+    # https-style (your bucket is in us-east-1)
+    if AWS_REGION == "us-east-1":
+        https_url = f"https://{S3_BUCKET}.s3.amazonaws.com/{key}"
+    else:
+        https_url = f"https://{S3_BUCKET}.s3.{AWS_REGION}.amazonaws.com/{key}"
+
+    return s3_url, https_url
