a5f5e8096e9d0fde26707492dc6cdc05ec676170
AutomatedRetailServices <arsgroup01@gmail.com>
2026-08-08 09:39:01 -0400

    Initialize CutSell API observability when configured

--- cutsell_app/__init__.py
+++ cutsell_app/__init__.py
@@ -1,1 +1,5 @@
 """CutSell clean mobile API package."""
+
+from cutsell_worker.commercial_observability import initialize_observability
+
+OBSERVABILITY_STATUS = initialize_observability(service="cutsell-api")
