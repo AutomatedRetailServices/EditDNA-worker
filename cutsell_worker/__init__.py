1576b255e14e58149bd159562307f3a0780463ec
AutomatedRetailServices <arsgroup01@gmail.com>
2026-08-13 17:30:35 -0400

    Install Clean Cut contract recovery at package load

--- cutsell_worker/__init__.py
+++ cutsell_worker/__init__.py
@@ -5,6 +5,9 @@ through versioned typed contracts defined in ``cutsell_worker.contracts``.
 """
 
 from .commercial_observability import initialize_observability
+from .clean_cut_retry import install_clean_cut_contract_recovery
+
+install_clean_cut_contract_recovery()
 
 __version__ = "0.1.0"
 OBSERVABILITY_STATUS = initialize_observability(service="cutsell-worker")
