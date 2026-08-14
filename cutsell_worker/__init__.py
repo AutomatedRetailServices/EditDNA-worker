4b0fd4008491420580a5be40887d12513cd8019d
AutomatedRetailServices <arsgroup01@gmail.com>
2026-08-14 03:27:18 -0400

    Enable visual self critique suffix trim

--- cutsell_worker/__init__.py
+++ cutsell_worker/__init__.py
@@ -7,9 +7,11 @@ through versioned typed contracts defined in ``cutsell_worker.contracts``.
 from .commercial_observability import initialize_observability
 from .clean_cut_retry import install_clean_cut_contract_recovery
 from .recording_process_context import install_recording_process_context_cleanup
+from .recording_suffix_trim import install_visual_self_critique_suffix_trim
 
 install_clean_cut_contract_recovery()
 install_recording_process_context_cleanup()
+install_visual_self_critique_suffix_trim()
 
 __version__ = "0.1.0"
 OBSERVABILITY_STATUS = initialize_observability(service="cutsell-worker")
