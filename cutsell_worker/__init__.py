e692f7e06c73b1b7918c8204bf0df991945ed0ff
AutomatedRetailServices <arsgroup01@gmail.com>
2026-08-14 03:25:54 -0400

    Enable recording process context cleanup

--- cutsell_worker/__init__.py
+++ cutsell_worker/__init__.py
@@ -6,8 +6,10 @@ through versioned typed contracts defined in ``cutsell_worker.contracts``.
 
 from .commercial_observability import initialize_observability
 from .clean_cut_retry import install_clean_cut_contract_recovery
+from .recording_process_context import install_recording_process_context_cleanup
 
 install_clean_cut_contract_recovery()
+install_recording_process_context_cleanup()
 
 __version__ = "0.1.0"
 OBSERVABILITY_STATUS = initialize_observability(service="cutsell-worker")
