419d7ed9371eeaeb5d7c3be41600d2e1517341bd
AutomatedRetailServices <arsgroup01@gmail.com>
2026-08-14 19:42:21 -0400

    Install deterministic recording-break cleanup

--- cutsell_worker/__init__.py
+++ cutsell_worker/__init__.py
@@ -10,12 +10,14 @@ from .recording_process_context import install_recording_process_context_cleanup
 from .recording_suffix_trim import install_visual_self_critique_suffix_trim
 from .dangling_delivery import install_dangling_delivery_cleanup
 from .internal_self_correction import install_internal_self_correction_trim
+from .recording_breaks import install_recording_break_cleanup
 
 install_clean_cut_contract_recovery()
 install_recording_process_context_cleanup()
 install_visual_self_critique_suffix_trim()
 install_dangling_delivery_cleanup()
 install_internal_self_correction_trim()
+install_recording_break_cleanup()
 
 __version__ = "0.1.0"
 OBSERVABILITY_STATUS = initialize_observability(service="cutsell-worker")
