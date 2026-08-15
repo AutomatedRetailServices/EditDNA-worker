85ab0b15db6f9cbc10c6be3f97203cafd1bd9d1a
AutomatedRetailServices <arsgroup01@gmail.com>
2026-08-15 03:30:49 -0400

    Install soft-frustration restart cleanup

--- cutsell_worker/__init__.py
+++ cutsell_worker/__init__.py
@@ -12,6 +12,7 @@ from .dangling_delivery import install_dangling_delivery_cleanup
 from .internal_self_correction import install_internal_self_correction_trim
 from .recording_breaks import install_recording_break_cleanup
 from .restart_questions import install_short_restart_question_cleanup
+from .frustrated_restart import install_soft_frustration_restart_cleanup
 
 install_clean_cut_contract_recovery()
 install_recording_process_context_cleanup()
@@ -20,6 +21,7 @@ install_dangling_delivery_cleanup()
 install_internal_self_correction_trim()
 install_recording_break_cleanup()
 install_short_restart_question_cleanup()
+install_soft_frustration_restart_cleanup()
 
 __version__ = "0.1.0"
 OBSERVABILITY_STATUS = initialize_observability(service="cutsell-worker")
