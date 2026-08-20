3e53dfb1bc5360df24dcee04f08649a8127ad211
AutomatedRetailServices <arsgroup01@gmail.com>
2026-08-20 05:00:01 -0400

    brain: apply sibling reconciliation after session grouping

--- cutsell_worker/__init__.py
+++ cutsell_worker/__init__.py
@@ -40,6 +40,7 @@ from .retry_group_integrity import install_retry_group_integrity
 from .attempt_boundary_integrity import install_attempt_boundary_integrity
 from .final_sibling_grouping import install_final_sibling_grouping
 from .session_grouping_bridge import install_session_grouping_bridge
+from .global_session_sibling_bridge import install_global_session_sibling_bridge
 from .selection_integrity import install_selection_integrity
 from .semantic_best_take_integrity import install_semantic_best_take_integrity
 
@@ -78,6 +79,7 @@ install_retry_group_integrity()
 install_attempt_boundary_integrity()
 install_final_sibling_grouping()
 install_session_grouping_bridge()
+install_global_session_sibling_bridge()
 install_selection_integrity()
 install_semantic_best_take_integrity()
 
